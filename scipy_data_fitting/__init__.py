27a59bcf977492b93799324fb4e2ed16e55cab73
Evan Sosenko <razorx@evansosenko.com>
2014-01-26 15:28:45 -0800

    Added initial base classes.

---
+++ scipy_data_fitting/__init__.py
@@ -0,0 +1,20 @@
+"""
+**Data Fitting with SciPy**
+
+This package is registered on the Python Package Index (PyPI) at
+[pypi.python.org/pypi/scipy-data_fitting](https://pypi.python.org/pypi/scipy-data_fitting).
+
+The source is hosted on GitHub at
+[github.com/razor-x/scipy-data_fitting](https://github.com/razor-x/scipy-data_fitting).
+
+This is the pdoc generated documentation,
+please see the README at either location above
+for more details about this package.
+"""
+
+from .data import Data
+from .fit import Fit
+from .model import Model
+from .plot import Plot
+
+__all__ = ['Data', 'Fit', 'Model', 'Plot']
---
+++ scipy_data_fitting/plot.py
@@ -0,0 +1,2 @@
+class Plot:
+    pass
