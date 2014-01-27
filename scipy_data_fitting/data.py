50a08db82ca4b4ec831905aed3b1fea85c30bd11
Evan Sosenko <razorx@evansosenko.com>
2014-01-27 11:44:53 -0800

    Added Data class with tests.

--- scipy_data_fitting/data.py
+++ scipy_data_fitting/data.py
@@ -1,2 +1,139 @@
+import numpy
+import scipy.constants
+
 class Data:
-    pass
+    """
+    Provides an interface to load data from files into
+    `numpy.Array` objects.
+
+    Example:
+
+        #!python
+        >>> data = Data()
+        >>> data.path = 'path/to/data.csv'
+        >>> data.scale = (1, 'kilo')
+        >>> data.array
+    """
+
+    def __init__(self, name=None):
+        self.name = name
+
+    @property
+    def name(self):
+        """
+        The identifier name for this object.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def path(self):
+        """
+        Path to the file containing data to load.
+        """
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        self._path = value
+
+    @property
+    def array(self):
+        """
+        Data as a `numpy.Array` in the form
+
+            #!python
+            [
+                [ x1, x2, x3, ... ],
+                [ y1, y2, y3, ...]
+            ]
+
+        The x and y values will be scaled according to `scipy_data_fitting.Data.scale`.
+        """
+        if not hasattr(self, '_array'): self._array = self.load_data()
+        return self._array
+
+    @array.setter
+    def array(self, value):
+        self._array = value
+
+    @property
+    def scale(self):
+        """
+        Tuple `(x_scale, y_scale)` that defines how to scale data
+        imported by `scipy_data_fitting.Data.load_data`.
+
+        Data is imported, then values are multiplied by the corresponding
+        scale value before retuning the array.
+
+        If a scale is specified as a string,
+        it will treated as a named physical constant
+        and converted to the corresponding number using `scipy.constants`.
+        """
+        if not hasattr(self, '_scale'): self._scale = (1, 1)
+        return self._scale
+
+    @scale.setter
+    def scale(self, value):
+        self._scale = [ self.get_scale(v) for v in value ]
+
+    @property
+    def genfromtxt_args(self):
+        """
+        Passed as keyword arguments to `numpy.genfromtxt`
+        when called by `scipy_data_fitting.Data.load_data`.
+
+        Default:
+
+            #!python
+            {
+                'unpack': True,
+                'delimiter': ',',
+                'usecols': (0 ,1),
+            }
+        """
+        if not hasattr(self, '_genfromtxt_args'):
+            self._genfromtxt_args = {
+                'unpack': True,
+                'delimiter': ',',
+                'usecols': (0 ,1),
+            }
+        return self._genfromtxt_args
+
+    @genfromtxt_args.setter
+    def genfromtxt_args(self, value):
+        self._genfromtxt_args = value
+
+    def load_data(self):
+        """
+        Loads data from `scipy_data_fitting.Data.path` using `numpy.genfromtxt`
+        and returns a `numpy.Array`.
+
+        Data is scaled according to `scipy_data_fitting.Data.scale`.
+        """
+        array = numpy.genfromtxt(self.path, **self.genfromtxt_args)
+
+        if self.scale == (1, 1):
+            return array
+        else:
+            return numpy.array([
+                array[0] * self.scale[0],
+                array[1] * self.scale[1]
+            ])
+
+    @staticmethod
+    def get_scale(value):
+        """
+        When `value` is a string, get the corresponding constant
+        from `scipy.constants`.
+        """
+        if type(value) is str:
+            if hasattr(scipy.constants, value):
+                return getattr(scipy.constants, value)
+            else:
+                return scipy.constants.physical_constants[value][0]
+        else:
+            return value
---
+++ test/test_data.py
@@ -0,0 +1,74 @@
+from scipy_data_fitting import Data
+
+import csv, os, tempfile
+from nose.tools import *
+from numpy.testing import *
+
+class TestData():
+
+    @classmethod
+    def setup_class(cls):
+        cls.raw_data = [
+            [1, 3, 8, 5],
+            [0, 2, 4, 7]
+        ]
+        cls.raw_data_pairs = list(zip(*cls.raw_data))
+
+        test_data_files = [
+            (',', []),
+            (',', ['x', 'y']),
+            ("\t", []),
+            ("\t", ['r', 't']),
+        ]
+        cls.data_files = [ cls.write_data(*args) for args in test_data_files ]
+
+    @classmethod
+    def teardown_class(cls):
+        for data_file in cls.data_files:
+            os.remove(data_file['filename'])
+
+    @classmethod
+    def write_data(cls, delimiter, headers):
+        filename = tempfile.mkstemp(prefix='scipy_data_fitting_test_')[1]
+        with open(filename, 'w+') as datafile:
+            writer = csv.writer(datafile, delimiter=delimiter)
+            if headers: writer.writerow(headers)
+            writer.writerows(cls.raw_data_pairs)
+
+        return {'delimiter': delimiter, 'headers': headers, 'filename': filename}
+
+    def test_load_data(self):
+        for data_file in self.data_files:
+            data = Data()
+            data.path = data_file['filename']
+            data.genfromtxt_args['delimiter'] = data_file['delimiter']
+            if data_file['headers']: data.genfromtxt_args['skip_header'] = 1
+            yield assert_allclose, data.load_data(), self.raw_data
+
+    def test_load_data_with_scale(self):
+        data_file = self.data_files[0]
+        data = Data()
+        data.path = data_file['filename']
+        data.genfromtxt_args['delimiter'] = data_file['delimiter']
+        data.scale = (2, 5)
+
+        raw_data_scaled = [
+            [ 2 * x for x in self.raw_data[0] ],
+            [ 5 * x for x in self.raw_data[1] ],
+        ]
+        assert_allclose(data.load_data(), raw_data_scaled)
+
+    def test_scale_with_numbers_gives_numbers(self):
+        data = Data()
+        data.scale = (1, 2)
+        assert_equal(data.scale, (1, 2))
+
+    def test_scale_with_strings_gives_numbers(self):
+        data = Data()
+        data.scale = ('kilo', 'milli')
+        assert_almost_equal(data.scale, (1000, 0.001))
+
+    def test_scale_with_mixed_gives_numbers(self):
+        data = Data()
+        data.scale = (2, 'Avogadro constant')
+        assert_almost_equal(data.scale, (2, 6.02214129e+23))
