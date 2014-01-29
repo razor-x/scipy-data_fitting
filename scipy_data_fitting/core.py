6aef0d6635bde284bb88f3b9ecc190b7b67652a4
Evan Sosenko <razorx@evansosenko.com>
2014-01-28 17:37:08 -0800

    Added core.prefix_factor.

--- scipy_data_fitting/core.py
+++ scipy_data_fitting/core.py
@@ -12,3 +12,14 @@ def get_constant(value):
             return scipy.constants.physical_constants[value][0]
     else:
         return value
+
+def prefix_factor(dictionary):
+    """
+    Searches `dictionary` for the key `prefix`.
+    If found, lookup the value with `scipy_data_fittings.core.get_constant`,
+    else return 1.
+    """
+    if 'prefix' in dictionary:
+        return get_constant(dictionary['prefix'])
+    else:
+        return 1
