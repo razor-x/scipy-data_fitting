b298b091304e57a73b6134051c0da224342e8618
Evan Sosenko <razorx@evansosenko.com>
2014-02-05 13:09:36 -0800

    For long_description, remove images since they are not supported.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 import os
 import subprocess
+import tempfile
 
 description = 'Data fitting system with SciPy.'
 long_description = description
@@ -14,8 +15,19 @@ except subprocess.CalledProcessError:
     pandoc = None
 
 if pandoc and os.path.exists('README.md'):
-    os.system('pandoc -s README.md -t rst -o README.txt')
+    temp_dir = tempfile.mkdtemp()
+    temp_path = os.path.join(temp_dir, 'README.md')
+
+    with open(temp_path, 'a') as f:
+        for line in open('README.md', 'r'):
+            # Remove lines that match Markdown images,
+            # they are not supported in `long_description`.
+            if not line.startswith('!['): f.write(line)
+
+    os.system('pandoc -s ' + temp_path + ' -t rst -o README.txt')
     long_description = open('README.txt').read()
+    os.remove(temp_path)
+    os.rmdir(temp_dir)
 
 setup(
     name = 'scipy-data_fitting',
